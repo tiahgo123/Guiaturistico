671927ed36f7ed7194ea37c2ac18f9d5237436db
sousatiago <tiago_s_15@hotmail.com>
2015-04-08 12:05:36 +0100

    Criar a classe para receber os valores da base dados e ficar guardados no arrayList...... Começar a implementção da parte de obter as coordenadas

--- app/src/main/java/ipvc/estg/guiaturistico/menu.java
+++ app/src/main/java/ipvc/estg/guiaturistico/menu.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
@@ -322,6 +323,15 @@ public class menu extends Activity {
                     verificarlinearOutro = false;
                 }
                 return false;
+            }
+        });
+
+        Button buttonIniciar = (Button) findViewById(R.id.buttonIniciar);
+        buttonIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Navegacao.class);
+                startActivity(intent);
             }
         });
 
